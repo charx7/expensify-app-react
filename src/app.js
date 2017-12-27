9be90d7637f552657c62a1b96fd07d44500d6cb8
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-26 18:35:52 -0600

    Estableciendo linkeos entre rutas de los componenetes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 //import IndecisionApp from './components/IndecisionApp';
 import './styles/styles.scss'; // Importacion de los archivos de CSS
 import 'normalize.css/normalize.css'; // Reseteo de los estilos de CSS con la nueva libreria 
-import {BrowserRouter, Route} from 'react-router-dom'; // Importaciones Necesarias para establecer el routeo
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'; // Importaciones Necesarias para establecer el routeo
 
 // Prueba
 const template = () => (
@@ -26,17 +26,50 @@ const Dummy3 = () => (
     <div>aiuda</div>
 );
 
+// Prueba
+const NoEncontrado = () => (
+    <div>
+        404 No Encontrado - 
+        <Link to = "/">
+             HOME
+        </Link>
+    </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify App</h1>
+        <NavLink to = "/" activeClassName = "esta-activo" exact={true}>
+            DASHBOARD
+        </NavLink>
+        <NavLink to = "/crear" activeClassName = "esta-activo">
+            Crear
+        </NavLink>
+        <NavLink to = "/editar" activeClassName = "esta-activo">
+            Editar
+        </NavLink>
+        <NavLink to = "/ayuda" activeClassName = "esta-activo">
+            Ayuda
+        </NavLink>
+    </header>
+);
+
 // Definimos las rutas basicas
 const rutas = (
     <BrowserRouter>
         <div>
-            <Route path = '/' component = {template} exact = {true}/>
-            <Route path= "/crear" component ={Dummy}/>
-            {/* Ruta de editar gasto*/}
-            <Route path= "/editar" component ={Dummy2}/>
-            {/* Ruta de aiuda*/}
-            <Route path= "/ayuda" component ={Dummy3}/>
-        </div>    
+            <Header/>
+            <Switch>
+                <Route path = '/' component = {template} exact = {true}/>
+                <Route path= "/crear" component ={Dummy}/>
+                {/* Ruta de editar gasto*/}
+                <Route path= "/editar" component ={Dummy2}/>
+                {/* Ruta de aiuda*/}
+                <Route path= "/ayuda" component ={Dummy3}/>
+                {/* Ruta para 404*/}
+                <Route component ={NoEncontrado}/>
+            </Switch>    
+        </div>
     </BrowserRouter>
 );
 
