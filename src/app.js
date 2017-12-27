04f5ab7de7573f55b601462cec076583fbfba53f
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-27 15:37:39 -0600

    Seguimos con cambios en la organizacion de REDUX

--- src/app.js
+++ src/app.js
@@ -5,15 +5,24 @@ import './styles/styles.scss'; // Importacion de los archivos de CSS
 import 'normalize.css/normalize.css'; // Reseteo de los estilos de CSS con la nueva libreria 
 import AppRouter from './routers/AppRouter';
 import configuraAlmacen from './almacen/configuraAlmacen'; // Importacion del Almacen REDUX y su modelo con reducers
+import { agregarGasto } from './acciones/gastos'; // Importaciones de acciones de gastos
+import { setFiltroTexto } from './acciones/filtros';
+import obtenerGastosVisibles from './selectores/selectorGastos'; // Importacion de la funcion que muestra los gastos visibles
 
 // acceso al modelo del almacen con REDUX
 const almacen = configuraAlmacen();
 
-console.log(almacen.getState());
-
 // Agregar dos Gastos
-// Usar una accion de filtros para texto de descripcion bill
-// Usar la funcion de mostrar gastos visibles 
+// Agregamos una serie de gastos ejemplo .displatch crea un objeto de accion
+const gastoUno = almacen.dispatch(agregarGasto({ descripcion: 'Water bill', monto: 100, creadoEn: -21000 }));
+const gastoDos = almacen.dispatch(agregarGasto({ descripcion: 'Gas bill', monto: 200, creadoEn: -21000 }));
+almacen.dispatch(setFiltroTexto('water'));
+
+// Para obtener el estado acual del almacen
+const estado = almacen.getState();
+// Para desplegar los gastos visibles segun los criterios de filtros
+const gastosVisibles = obtenerGastosVisibles(estado.gastos, estado.filtros);
+console.log(gastosVisibles);
 
 // Rendereo de toda la aplicacion usando el componente padre IndecisionApp
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
