be58c42c1f67346dd08538d6bd75aafa3dce2f8b
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-28 15:27:07 -0600

    Aniadidos links a objetos individuales para editar

--- src/components/ListaGastosItem.js
+++ src/components/ListaGastosItem.js
@@ -2,12 +2,15 @@
 // renderear descripcion monto y creado en
 import React from  'react';
 import { connect } from 'react-redux'; // Importador de la conexion al almacen
+import { Link } from 'react-router-dom'; // Importacion para poder usar un componente de Link
 import { removerGasto } from '../acciones/gastos'; // Importador de acciones de gastos para remover
 
 // Componente funcional de opcion
 const ListaGastoItem = (props) => (
     <div>
-        <h3>{props.currentDescripcion}</h3>
+        <Link to={`/editar/${props.currentId}`}>
+            <h3>{props.currentDescripcion}</h3>
+        </Link>
         <p> El monto del gasto es {props.currentMonto} - Creado en: {props.currentCreadoEn} </p>
         <button 
             onClick = { () => {
