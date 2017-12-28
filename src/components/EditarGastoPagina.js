b6b064813ed40448c21972ee95c4932a1bec2f98
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-28 15:35:53 -0600

    Agregada pasar el objeto al componente de EditarGasto por medio de una conexion con el almacen

--- src/components/EditarGastoPagina.js
+++ src/components/EditarGastoPagina.js
@@ -1,12 +1,12 @@
 // Importaciones de React
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Importacion de connect de React redux
+import { connect } from 'react-redux';
 
 // Componente de Editar Gasto
 const EditarGastoPagina = (props) => {
-    
     console.log(props);
-
     return (
         <div>
             Editando el Gasto con ID de {props.match.params.id}
@@ -14,4 +14,16 @@ const EditarGastoPagina = (props) => {
     );
 };
 
-export default EditarGastoPagina;
+// Mapeo del estado del almacen a props
+const mapeoEstadoAProps = (estado, props) => {
+    return {
+        // Buscamos en el arreglo de gastos del almacen para que nos devuelva el objeto con las propiedades indicadas
+        gasto: estado.gastos.find( (gasto) => {
+            // Verifica si el argumento pasado tiene el mismo id que el del almacen
+            return gasto.id == props.match.params.id;
+        })
+    };
+};
+
+// Conexion con el Store
+export default connect(mapeoEstadoAProps) (EditarGastoPagina);
