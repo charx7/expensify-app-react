d3570b0dc43f6a3c7bb29c48cdfcb67ecd3f8ce3
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-28 12:55:09 -0600

    Agregada funcionalidad de cambio de estado del componente de monto usando REGEX y verificando el input del usuario

--- src/components/FormaGasto.js
+++ src/components/FormaGasto.js
@@ -6,7 +6,8 @@ export default class FormaGasto extends React.Component{
     // Estado del componente que mantiene track de los cambios de los inputs y el textarea
     state = {
         descripcion: '',
-        nota: ''
+        nota: '',
+        monto: ''
     };
     // Metodo que se encarga de cambiar el estado de descripcion del componente
     cambioDescripcion = (e) => {
@@ -24,6 +25,17 @@ export default class FormaGasto extends React.Component{
         // Cambio del estado del componente segun el valor del area de texto
         this.setState( () => ({ nota: notaForma }));
     }
+    // Metodo que se encarga del cambio de estado de monto
+    cambioMonto = (e) => {
+        // Jalamos el valor del input
+        const montoForma = e.target.value;
+        // Logica de verificacion del input el non-sense es una regular-expression que se encarga de verificar si es un
+        // numero con 2 decimales maximo
+        if( montoForma.match( /^\d*(\.\d{0,2})?$/ )) {
+            // Cambio del estado
+            this.setState( () => ({ monto: montoForma}));
+        }
+    } 
     // Rendereo del componente
     render () {
         return (
@@ -36,8 +48,10 @@ export default class FormaGasto extends React.Component{
                     value = {this.state.descripcion}
                     onChange = {this.cambioDescripcion}
                  />
-                 <input type="number"
+                 <input type="text"
                     placeholder='monto'
+                    value = {this.state.monto}
+                    onChange = {this.cambioMonto}
                   />
                 <textarea 
                     placeholder='Agregar Nota para tu gasto (opcional)'
