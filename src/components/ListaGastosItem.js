160ee27f8d099d5dad509034542ed4f8a3c71f8e
Carlos Huerta <carlos.huertaso@udlap.mx>
2017-12-30 19:31:24 -0600

    Agregado formateo de Monto y Fecha en desplegado de la lista de items

--- src/components/ListaGastosItem.js
+++ src/components/ListaGastosItem.js
@@ -4,6 +4,8 @@ import React from  'react';
 import { connect } from 'react-redux'; // Importador de la conexion al almacen
 import { Link } from 'react-router-dom'; // Importacion para poder usar un componente de Link
 import { removerGasto } from '../acciones/gastos'; // Importador de acciones de gastos para remover
+import moment from 'moment'; // Importaciones de moment para formato de fechas
+import numeral from 'numeral'; // Importaciones de node_modules para formato de montos
 
 // Componente funcional de opcion
 const ListaGastoItem = (props) => (
@@ -11,7 +13,10 @@ const ListaGastoItem = (props) => (
         <Link to={`/editar/${props.currentId}`}>
             <h3>{props.currentDescripcion}</h3>
         </Link>
-        <p> El monto del gasto es {props.currentMonto} - Creado en: {props.currentCreadoEn} </p>
+        <p> 
+            El monto del gasto es {numeral(props.currentMonto / 100).format('$0,0.00')} - {' '}
+            Creado en: {moment(props.currentCreadoEn).format('MMMM Do, YYYY')} 
+        </p>
         {/* MARCADO COMO COMENTARIO PORQUE FUE MOVIDO AL COMPONENTE DE EDITAR         
         <button 
             onClick = { () => {
